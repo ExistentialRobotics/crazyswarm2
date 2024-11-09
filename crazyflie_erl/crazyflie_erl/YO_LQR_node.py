50c5ff1fe38c9115cf322ebf91a85c313218d695
Rohan Nagrik <rohannagrik25@gmail.com>
2024-11-08 19:17:24 -0800

    update control node

--- crazyflie_erl/crazyflie_erl/YO_LQR_node.py
+++ crazyflie_erl/crazyflie_erl/YO_LQR_node.py
@@ -4,6 +4,9 @@ from rclpy.qos import QoSProfile, ReliabilityPolicy
 from geometry_msgs.msg import Twist, PoseStamped
 from motion_capture_tracking_interfaces.msg import NamedPoseArray
 from YO_Controller import YO_Controller
+import time
+import numpy as np
+import tf_transformations
 
 
 #################################################################
@@ -16,12 +19,15 @@ class Crazyswarm2ERLCommander(Node):
     def __init__(self):
         super.__init__('CS2ERLCommander_node')
 
-        self.controller = self.define_controller()
+        self.controller, self.dt = self.define_controller()
         self.cf_locked = True
 
         qos_profile = QoSProfile(reliability=ReliabilityPolicy.BEST_EFFORT, depth=10)
         self.subscriber = self.create_subscription(NamedPoseArray, '/cf231/pose',self.callback,qos_profile)
         self.publisher = self.create_publisher(Twist, '/cf231/cmd_vel_legacy', 10)
+
+        self.last_thrust = 0
+        self.last_pos = np.zeros(3)
 
 
     def define_controller(self):
@@ -41,12 +47,10 @@ class Crazyswarm2ERLCommander(Node):
                     max_vel_error,max_pos_error,max_yaw_error,max_pitch_roll_error]
         controller = YO_Controller(c_params)
        
-        return controller
+        return controller, CTRL_TIMESTEP
 
 
     def callback(self,msg:PoseStamped):
-        
-        ## INCOMPLETE
         
         control_msg = Twist()
 
@@ -55,10 +59,25 @@ class Crazyswarm2ERLCommander(Node):
             for i in range(5):
                 self.publisher.publish(control_msg)
 
-        command = self.controller.get_singlecf_control()
+        ori = tf_transformations.euler_from_quaternion([msg.pose.orientation.x,
+                                                        msg.pose.orientation.y,
+                                                        msg.pose.orientation.z])
+        curr_pos = np.array([msg.pose.position.x,
+                             msg.pose.position.y,
+                             msg.pose.position.z])
+        curr_vel = (curr_pos - self.last_pos)/self.dt
+        
+        state = [*ori,self.last_thrust,*curr_vel,*curr_pos]
+        command = self.controller.get_singlecf_control(state)
+
+        control_msg.linear.y, control_msg.linear.x, control_msg.angular.z = command[:-1]
+        control_msg.linear.z = int(command[-1])
 
         self.publisher.publish(control_msg)
-        self.get_logger().info(f'Published {control_msg}')
+        self.get_logger().info(f'Published {control_msg} at time {time.time()}')
+
+        self.last_pos = curr_pos
+        self.last_thrust = control_msg.linear.z
         
 
 
